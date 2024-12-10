7d7b916b5000259002ae325921afd272972215ed
dganicolas <dganicolas@rafaelalberti.es>
2024-12-10 21:16:32 +0100

    primer contacto

---
+++ piedraPapelTijera/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// creado por nicolas de gomar
+class ThreadTest 
+{ 
+
+ 
+  static void Go() 
+  { 
+    Console.WriteLine ("hello!"); 
+  } 
+} 
+
+  static void Main()  
+  { 
+    Thread t = new Thread (Go); 
+ 
+    t.Start();   // Run Go() on the new thread. 
+    Go();        // Simultaneously run Go() in the main thread. 
+  } 
